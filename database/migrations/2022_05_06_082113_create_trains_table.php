9bfbe3cf5ca407d7845debb7772c70c26a2800cd
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-06 11:43:50 +0200

    first commit

---
+++ database/migrations/2022_05_06_082113_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100)->nullable(true);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->integer('codice_treno');
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains') ;
+    }
+}
